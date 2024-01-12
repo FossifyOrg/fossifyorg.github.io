fcc1d111f628d497a3aa63388d7759f6cf3207e6
ronniedroid <ronnie.nissan@proton.me>
2024-01-12 15:42:54 +0300

    added the header bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.fossify.org/'
+  site: 'https://www.fossify.org/',
+  integrations: [icon()]
 });
