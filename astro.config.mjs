4a86d11904064b067d91bfeab1af9fe8a121d018
ronniedroid <ronnie.nissan@proton.me>
2024-01-12 17:01:12 +0300

    removed problomatic svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.fossify.org/',
+  site: 'https://ronniedroid.github.io/fossifyorg.github.io',
   integrations: [icon()]
 });
