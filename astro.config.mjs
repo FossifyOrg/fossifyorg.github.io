5e4c4cc48c557cddfa9b76518fde1641490063cb
ronniedroid <ronnie.nissan@proton.me>
2024-01-12 17:08:26 +0300

    trying to fix github page not loading css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ronniedroid.github.io/fossifyorg.github.io',
+  site: 'https://ronniedroid.github.io',
+  base: 'fossifyorg.github.io',
   integrations: [icon()]
 });
